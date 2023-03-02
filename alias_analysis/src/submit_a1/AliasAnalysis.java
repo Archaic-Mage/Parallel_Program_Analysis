6776442f0574bf260216e83addecaa93d9d842e8
Archaic-Mage <sohasoham811@gmail.com>
2023-03-03 00:25:57 +0530

    complete worklist algo and four load cases

--- alias_analysis/src/submit_a1/AliasAnalysis.java
+++ alias_analysis/src/submit_a1/AliasAnalysis.java
@@ -31,6 +31,16 @@ public class AliasAnalysis extends BodyTransformer{
 		//@ indicates any field
         HashMap<String, Set<String>> rho_data;
         HashMap<String, HashMap<String, Set<String>>> sigma_data;
+		Out() {
+			rho_data = new HashMap<>();
+			sigma_data = new HashMap<>();
+		}
+        public Set<String> rho(String a) {
+            return rho_data.get(a);
+        }
+        public Set<String> sigma(String ref, String field) {
+            return sigma_data.get(ref).get(field);
+        }
 		Out union(Out a) {
 			//Union of rho_data
 			for(String name: a.rho_data.keySet()) {
@@ -60,16 +70,31 @@ public class AliasAnalysis extends BodyTransformer{
 			}
 			return this;
 		}
-		Out() {
-			rho_data = new HashMap<>();
-			sigma_data = new HashMap<>();
+		Boolean isEqual(Out a) {
+			//comparing stack space
+			if(a.rho_data.size() != this.rho_data.size()) {
+				return false;
+			}
+			for(String name: a.rho_data.keySet()) {
+				if(!this.rho_data.containsKey(name)) return false;
+				else if(!a.rho(name).equals(this.rho(name))) return false;
+			}
+
+			//comparing heap space
+			if(a.sigma_data.size() != this.sigma_data.size()) return false;
+			for(String ref: a.sigma_data.keySet()) {
+				if(!this.sigma_data.containsKey(ref)) return false;
+				else {
+					if(a.sigma_data.get(ref).size() != this.sigma_data.get(ref).size()) return false;
+					for(String field: a.sigma_data.keySet()) {
+						if(!this.sigma_data.get(ref).containsKey(field)) return false;
+						else if(!a.sigma(ref, field).equals(this.sigma(ref, field))) return false;
+					}
+				}
+			}
+
+			return true;
 		}
-        public Set<String> rho(String a) {
-            return rho_data.get(a);
-        }
-        public Set<String> sigma(String ref, String field) {
-            return sigma_data.get(ref).get(field);
-        }
     }
     HashMap<Unit, Out> get_out;
 	Out union(List<Out> outs) {
@@ -103,7 +128,8 @@ public class AliasAnalysis extends BodyTransformer{
         }
 		//copy type a = b
 		if(left instanceof JimpleLocal && right instanceof JimpleLocal) {
-			Set<String> replace = new HashSet<>(in.rho(right.toString()));
+			Set<String> replace = new HashSet<>();
+			if(in.rho(right.toString()) != null) replace.addAll(in.rho(right.toString()));
 			in.rho_data.put(left.toString(), replace);
 			return in;
 		}
@@ -118,11 +144,13 @@ public class AliasAnalysis extends BodyTransformer{
 			return in;
 		}
 		//assign return of a call a = b.foo(args...)
-//		if(left instanceof JimpleLocal && right instanceof InvokeExpr) {
-//
-//		}
-        Out temp = new Out();
-        return temp;
+		if(left instanceof JimpleLocal && right instanceof InvokeExpr) {
+			//a points to Og
+			Set<String> replace = new HashSet<>();
+
+		}
+
+        return in;
     }
 
 	Out process_invoke(Unit s, UnitGraph g) {
@@ -135,6 +163,7 @@ public class AliasAnalysis extends BodyTransformer{
 	void debug(Object s) { System.out.println(s); }
 
 	void print_out(Out o) {
+		debug("------------------");
 		debug("Stack Space Data");
 		for(String name: o.rho_data.keySet()) {
 			for(String ref: o.rho(name)) {
@@ -150,6 +179,7 @@ public class AliasAnalysis extends BodyTransformer{
 				}
 			}
 		}
+		debug("------------------");
 	}
 	//create two out and checks union
 	void test() {
@@ -185,7 +215,6 @@ public class AliasAnalysis extends BodyTransformer{
 		fields.put("test", temp);
 		b.sigma_data.put("O6", fields);
 
-
 		debug("a before union");
 		print_out(a);
 		debug("b before union");
@@ -206,29 +235,75 @@ public class AliasAnalysis extends BodyTransformer{
 		debug(arg0.getMethod().getDeclaringClass());
 		UnitGraph g = new BriefUnitGraph(arg0);
 
-		for(Unit s : g) {
-			if(s instanceof AssignStmt) {
-				debug(s);
-				Value l = ((AssignStmt) s).getLeftOp();
-				Value r = ((AssignStmt) s).getRightOp();
-				debug(l + ": " + l.getClass());
-//				if(l instanceof JInstanceFieldRef) {
-//					System.out.println(((JInstanceFieldRef) l).getBase());
-//					System.out.println(((JInstanceFieldRef) l).getField().getName());
-//				}
-				System.out.println(r + ": " + r.getClass());
-//				if(r instanceof JInstanceFieldRef) {
-//					System.out.println(((JInstanceFieldRef) r).getBase());
-//					System.out.println(((JInstanceFieldRef) r).getField().getName());
-//				}
-//				if(r instanceof JVirtualInvokeExpr) {
-//					System.out.println(((JVirtualInvokeExpr) r).getBase());
-//					System.out.println(((JVirtualInvokeExpr) r).getMethod().getName());
-//					System.out.println(((JVirtualInvokeExpr) r).getArgs());
-//				}
-				System.out.println("-----");
+		//initialize everything with top
+		get_out = new HashMap<>();
+		for(Unit s: g) {
+			Out out = new Out();
+			get_out.put(s, out);
+		}
+
+		Stack<Unit> work_list = new Stack<>();
+		Set<Unit> visited = new HashSet<>();
+		work_list.push(g.getHeads().get(0));
+
+		while(!work_list.isEmpty()) {
+			Unit to_process = work_list.pop();
+			visited.add(to_process);
+			Out out = new Out();
+			//debug(to_process);
+			if(to_process instanceof AssignStmt) {
+				out = process_assign(to_process, g);
+			} /*else if(((Stmt) to_process).containsInvokeExpr()) {
+				out = process_invoke(to_process, g);
+			} */else {
+				for(Unit pred: g.getPredsOf(to_process)) {
+					out = out.union(get_out.get(pred));
+				}
+			}
+
+			//print_out(out);
+
+			if(!get_out.get(to_process).isEqual(out)) {
+				get_out.put(to_process, out);
+				for(Unit suc: g.getSuccsOf(to_process)) {
+					work_list.push(suc);
+				}
+			} else {
+				for(Unit suc: g.getSuccsOf(to_process)) {
+					if(!visited.contains(suc)) work_list.push(suc);
+				}
 			}
 		}
+
+		print_out(get_out.get(g.getTails().get(0)));
+
+//		for(Unit s: get_out.keySet()) {
+//			print_out(get_out.get(s));
+//		}
+
+//		for(Unit s : g) {
+//			if(s instanceof AssignStmt) {
+//				debug(s);
+//				Value l = ((AssignStmt) s).getLeftOp();
+//				Value r = ((AssignStmt) s).getRightOp();
+//				debug(l + ": " + l.getClass());
+////				if(l instanceof JInstanceFieldRef) {
+////					System.out.println(((JInstanceFieldRef) l).getBase());
+////					System.out.println(((JInstanceFieldRef) l).getField().getName());
+////				}
+//				System.out.println(r + ": " + r.getClass());
+////				if(r instanceof JInstanceFieldRef) {
+////					System.out.println(((JInstanceFieldRef) r).getBase());
+////					System.out.println(((JInstanceFieldRef) r).getField().getName());
+////				}
+////				if(r instanceof JVirtualInvokeExpr) {
+////					System.out.println(((JVirtualInvokeExpr) r).getBase());
+////					System.out.println(((JVirtualInvokeExpr) r).getMethod().getName());
+////					System.out.println(((JVirtualInvokeExpr) r).getArgs());
+////				}
+//				System.out.println("-----");
+//			}
+//		}
 
 		for(AliasQuery q: A1.queryList) {
 			if(q.getClassName().equals(arg0.getMethod().getDeclaringClass().toString())) {
